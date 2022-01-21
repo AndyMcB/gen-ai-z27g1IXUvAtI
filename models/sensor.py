b35deceddecf768637b09f016d181446b887eee8
Andy <andy@Andys-MacBook-Pro.local>
2022-01-21 21:57:36 +0000

    Create basic models for app entities

---
+++ models/sensor.py
@@ -0,0 +1,14 @@
+class Sensor:
+    uuid = None
+    country = None
+    city = None 
+
+    def __init__(self, uuid, country, city):
+        self.uuid = uuid
+        self.country = country
+        self.city = city
+
+    
+
+
+
