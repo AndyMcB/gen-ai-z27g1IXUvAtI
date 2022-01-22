e3437fc18acc4a0bd15d97ba6a4ff22859f80e93
Andy <andy@Andys-MacBook-Pro.local>
2022-01-22 16:03:21 +0000

    Add endpoint which can create sensors

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from flask import Flask, jsonify
-
+from crypt import methods
+from json.tool import main
+from flask import Flask, jsonify, request
+from models.sensor import Sensor
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return jsonify(
-        resp = "hello"
+        hello = "World"
     )
 
-@app.route('/greet', defaults={'name': 'Programmer'})
-@app.route('/greet/<name>')
-def greet(name):
-    return jsonify(
-        name=name
-    )
+@app.route("/sensor/create", methods=["POST"])
+def create_sensor():
+    request_data = request.get_json()
+    try:
+        sensor = Sensor(request_data["uuid"], request_data["country"], request_data["city"])
+        result = sensor.save()
+        print(result)
+ 
+        if result["success"]:
+            return jsonify(code=200, msg=result["msg"])
+        else:
+            return jsonify(code=500, msg=result["msg"])
+    except KeyError:
+        return jsonify(code = 400, msg = "Attribute key missing")
