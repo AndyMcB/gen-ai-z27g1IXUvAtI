a409587ef3e4d928eb21962a05b8577f21bcfbb4
Andrew <1andymc1@gmail.com>
2022-01-22 17:34:27 +0000

    Add functionality to query sensors from DB and instanciate models

--- models/sensor.py
+++ models/sensor.py
@@ -1,19 +1,43 @@
 from database.manager import DatabaseManager
 
 class Sensor:
-    def __init__(self, uuid, country, city):
+    def __init__(self, uuid, country, city, created_at=None):
         self.uuid = uuid
         self.country = country
         self.city = city
-        self.created_at = None
+        self.created_at = created_at
 
     def save(self):
         db_manager = DatabaseManager()
         query_string = "INSERT INTO Sensors (uuid, country, city) values(?, ?, ?)"
         return db_manager.execute_query(query_string, [self.uuid, self.country, self.city])
 
+    ### Static Methods
+    @staticmethod
+    def get_all_sensors():
+        db_manager = DatabaseManager()
+        query = "SELECT * FROM Sensors"
+        res = db_manager.execute_query(query)
         
-    
+        sensors = map(lambda row: Sensor(row[1], row[2], row[3], row[4]), 
+            res["data"]
+        )
+        
+        return sensors
+
+    @staticmethod
+    def get_sensor_by_uuid(uuid):
+        db_manager = DatabaseManager()
+        query = "SELECT * FROM Sensors WHERE uuid = ?"
+        res = db_manager.execute_query(query, [uuid])
+
+        sensor = {}
+        if res["data"]:
+            row = res["data"][0]
+            sensor = Sensor(row[1], row[2], row[3], row[4])
+        
+        return sensor
+        
 
 
 
