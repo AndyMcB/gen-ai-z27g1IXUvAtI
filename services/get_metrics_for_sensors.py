6e1f6b7a0235bb32629924d57914b8fc16cbdbe0
Andrew <1andymc1@gmail.com>
2022-01-25 18:47:02 +0000

    Cleanup returns in averages service and handle missing parameters in service

--- services/get_metrics_for_sensors.py
+++ services/get_metrics_for_sensors.py
@@ -10,22 +10,24 @@ class GetMetricsForSensors:
         self.metrics = self.__parse_arg(metrics)
         self.start_day = self.__parse_day(start_day)
         self.end_day = self.__parse_day(end_day)
+        self.averages = {}
 
     def calculate(self):
         sensor_data = self.get_sensor_data()
-        sensor_data = self.filter_data_by_dates(sensor_data)
+        if not sensor_data:
+            return self.get_return_value()
         
-        averages = {}
-        for metric in self.metrics:
-            averages[metric] = self.get_average_for_metric(metric, sensor_data)
+        sensor_data = self.filter_data_by_dates(sensor_data)
+        if not sensor_data:
+            return self.get_return_value()
+        
+        if not self.metrics:
+            return self.get_return_value()
 
-        return {
-            "sensors": self.sensor_uuids,
-            "metrics": self.metrics,
-            "start_day": self.start_day,
-            "end_day": self.end_day,
-            "averages": averages
-        }
+        for metric in self.metrics:
+            self.averages[metric] = self.get_average_for_metric(metric, sensor_data)
+
+        return self.get_return_value()
 
     def get_sensor_data(self):
         sensors = list(map(lambda uuid: SensorData.get_sensor_data_by_sensor_uuid(uuid), self.sensor_uuids))
@@ -34,11 +36,18 @@ class GetMetricsForSensors:
     def filter_data_by_dates(self, sensor_data):
         return [ data for data in sensor_data
             if self.start_day <= self.__parse_datetime(data.created_at) <= self.end_day]
-
+            
     def get_average_for_metric(self, metric, sensor_data):
         return reduce(lambda x,y: x+y, [vars(data)[metric] for data in sensor_data]) / len(sensor_data)
 
-
+    def get_return_value(self):
+        return {
+            "sensors": self.sensor_uuids,
+            "metrics": self.metrics,
+            "start_day": self.start_day,
+            "end_day": self.end_day,
+            "averages": self.averages
+        }
     ### Private Methods
 
     def __parse_arg(self, arg):
