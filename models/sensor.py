a8a183f4a59581c9cb0cf4a09313d510598f3bd8
Andrew <1andymc1@gmail.com>
2022-01-25 18:44:20 +0000

    Add some input validation on models

--- models/sensor.py
+++ models/sensor.py
@@ -6,11 +6,35 @@ class Sensor:
         self.country = country
         self.city = city
         self.created_at = created_at
+        self.saved = created_at != None
+
+    @property
+    def uuid(self):
+        return self._uuid
+
+    @uuid.setter
+    def uuid(self, value):
+        if Sensor.uuid_exists(value):
+            raise ValueError("UUID already exists in DB")
+        self._uuid = value
 
     def save(self):
         db_manager = DatabaseManager()
         query_string = "INSERT INTO Sensors (uuid, country, city) values(?, ?, ?)"
-        return db_manager.execute_query(query_string, [self.uuid, self.country, self.city])
+        res = db_manager.execute_query(query_string, [self.uuid, self.country, self.city])    
+        if res["success"]:
+            self.saved = True
+            return True
+        return False
+
+    def delete(self):
+        db_manager = DatabaseManager()
+        query_string = "DELETE FROM Sensors WHERE uuid = ?"
+        res = db_manager.execute_query(query_string, [self.uuid])    
+        if res["success"]:
+            self.saved == False
+            return True
+        return False
 
     ### Static Methods
     @staticmethod
@@ -19,10 +43,9 @@ class Sensor:
         query = "SELECT * FROM Sensors"
         res = db_manager.execute_query(query)
         
-        sensors = map(lambda row: Sensor(row[1], row[2], row[3], row[4]), 
+        sensors = list(map(lambda row: Sensor(row[1], row[2], row[3], row[4]), 
             res["data"]
-        )
-        
+        ))
         return sensors
 
     @staticmethod
@@ -31,12 +54,20 @@ class Sensor:
         query = "SELECT * FROM Sensors WHERE uuid = ?"
         res = db_manager.execute_query(query, [uuid])
 
-        sensor = {}
+        sensor = None
         if res["data"]:
             row = res["data"][0]
             sensor = Sensor(row[1], row[2], row[3], row[4])
-        
         return sensor
+
+    @staticmethod
+    def uuid_exists(uuid):
+        db_manager = DatabaseManager()
+        query = "SELECT * FROM Sensors WHERE uuid = ?"
+        res = db_manager.execute_query(query, [uuid])
+        if res["data"]:
+            return True
+        return False
         
 
 
--- models/sensor_data.py
+++ models/sensor_data.py
@@ -1,4 +1,5 @@
 from database.manager import DatabaseManager
+from models.sensor import Sensor
 
 class SensorData:
     def __init__(self, sensor_uuid, temp, humidity, wind_speed, created_at=None):
@@ -7,22 +8,38 @@ class SensorData:
         self.humidity = humidity
         self.wind_speed = wind_speed
         self.created_at = created_at
+        self.saved = created_at != None
 
     def save(self):
         db_manager = DatabaseManager()
         query_string = "INSERT INTO SensorData (sensor_uuid, temperature, humidity, wind_speed) values(?, ?, ?, ?)"
-        return db_manager.execute_query(query_string, [self.sensor_uuid, self.temp, self.humidity, self.wind_speed])
+        res = db_manager.execute_query(query_string, [self.sensor_uuid, self.temp, self.humidity, self.wind_speed])
+        if res["success"]:
+            self.saved = True
+            return True
+        return False
+
+    @property
+    def sensor_uuid(self):
+        return self._sensor_uuid
+
+    @sensor_uuid.setter
+    def sensor_uuid(self, value):
+        if Sensor.uuid_exists(value):
+            self._sensor_uuid = value
+        else:
+            raise ValueError("Sensor UUID does not exist")
+
 
     ### Static Methods
     @staticmethod
     def get_all_sensor_data():
         db_manager = DatabaseManager()
         query = "SELECT * FROM SensorData"
-        res = db_manager.execute_query(query)
-        
-        sensor_data = map(lambda row: SensorData(row[1], row[2], row[3], row[4], row[5]), 
+        res = db_manager.execute_query(query) 
+        sensor_data = list(map(lambda row: SensorData(row[1], row[2], row[3], row[4], row[5]), 
             res["data"]
-        )
+        ))
         return sensor_data
 
     @staticmethod
@@ -31,7 +48,7 @@ class SensorData:
         query = "SELECT * FROM SensorData WHERE sensor_uuid = ?"
         res = db_manager.execute_query(query, [uuid])
 
-        sensor_data = map(lambda row: SensorData(row[1], row[2], row[3], row[4], row[5]), 
+        sensor_data = list(map(lambda row: SensorData(row[1], row[2], row[3], row[4], row[5]), 
             res["data"]
-        )
+        ))
         return sensor_data
